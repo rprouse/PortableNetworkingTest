c61d884cbd62e9f62fa01eab2756d1915ff10aa8
Rob Prouse <rob@prouse.org>
2014-11-14 16:59:57 -0500

    Spike to test HTTP networking using portable libraries

---
+++ NetworkClient/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using PortableClient;
+
+namespace NetworkClient
+{
+    class ConsoleRenderer : IRenderer
+    {
+        #region Implementation of IRenderer
+
+        public void RenderStream( Stream stream )
+        {
+            using ( var reader = new StreamReader( stream ) )
+            {
+                Console.Write( reader.ReadToEnd() );
+            }
+        }
+
+        #endregion
+    }
+
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            var client = new WebClient( new ConsoleRenderer() );
+            client.HttpGet( @"http://www.alteridem.net/" );
+
+            Console.WriteLine("*** Waiting for response ***");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PortableClient/WebClient.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+
+namespace PortableClient
+{
+    public interface IRenderer
+    {
+        void RenderStream( Stream stream );
+    }
+
+    public class WebClient
+    {
+        private readonly IRenderer _renderer;
+
+        public WebClient( IRenderer renderer )
+        {
+            _renderer = renderer;
+        }
+
+        public void HttpGet( string url )
+        {
+            WebRequest request = WebRequest.Create( url );
+
+            request.BeginGetResponse( state =>
+            {
+                var webRequest = state.AsyncState as HttpWebRequest;
+                if ( webRequest == null ) return;
+
+                try
+                {
+                    WebResponse response = webRequest.EndGetResponse( state );
+                    _renderer.RenderStream( response.GetResponseStream() );
+                }
+                catch ( Exception e )
+                {
+                    Debug.WriteLine( e );
+                }
+
+            }, request );
+        }
+    }
+}
